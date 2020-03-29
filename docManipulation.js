7e0aaf8c9bf2d290b3f10171eee5005c4d006750
Ethan <e.earlie@hotmail.com>
2020-03-28 23:16:42 -0400

    Updating full site with new code for react tabs and d3 plots

--- docManipulation.js
+++ docManipulation.js
@@ -1,6 +1,5 @@
 // Function to append div elemnts to an HTML document with an existing div element with id='oldDivID'.
-// This function is useful for when you have a variable amount of plots to display on the page:
-
+// Useful for when you have a variable amount of plots to display on the page:
 addElement = function(newDivID, oldDivID) { 
   // create a new div element 
   var newDiv = document.createElement("div"); 
@@ -12,12 +11,12 @@ addElement = function(newDivID, oldDivID) {
 }
 
 
-// This function will remove the current div elements if they exist:
-removeElements = function() {
+// Function to remove the current div elements if they exist:
+removeDIVelements = function() {
   var i = 1;
   var continueBool = true;
   while (continueBool == true) {
-    divToRemove = document.getElementById("div"+i);
+    divToRemove = document.getElementById("div" + i);
     if(divToRemove) {
       $(divToRemove).remove();
       i++;
@@ -27,32 +26,45 @@ removeElements = function() {
   };
 };
 
+// Function to remove the current svg elements if they exist:
+removeSVGelements = function() {
+  var i = 0;
+  var continueBool = true;
+  while (continueBool == true) {
+    svgToRemove = document.getElementById("svgHeatMap");
+    if(svgToRemove) {
+      $(svgToRemove).remove();
+      i++;
+    } else {
+      var continueBool = false;
+    };
+  };
+};
 
-// This functino is to display the error message:
+
+// Function to display the error message:
 showError = function(errorType) {
   // Create div1 and set it to be alert class:
   addElement('div1','div0');
   var divElement = document.getElementById('div1');
   divElement.className = 'alert';
 
-  // Create span clone from span0 to add to div1:
+  // Creates span clone from span0 to add to div1:
   var span = document.getElementById('span0');
   var spanElement = span.cloneNode(true);
   spanElement.setAttribute('id','span1');
   divElement.appendChild(spanElement);
 
-  // Add the error message to the div:
+  // Adds the error message to the div:
   if (errorType == 'geneError') {
     divElement.innerHTML += "Error: ".bold() + "Invalid Gene Fields for Query";
   } else if (errorType == 'cohortError') {
     divElement.innerHTML += "Error: ".bold() + "Invalid Cohort Fields for Query";
-  } else if (errorType == 'fireBrowseError') {
-    divElement.innerHTML += "Error: ".bold() + "The Firebrowse API is currently offline. Please check back later.";
   };
 };
 
 
-// This function displays a warning for genes that don't have mRNA-Seq data:
+// Function to display a warning for genes that don't have mRNA-Seq data:
 showWarning = function(emptyGeneArray_arg) {
   // Create div1 and set it to be warning class:
   //addElement('div1','div0');
@@ -74,21 +86,31 @@ showWarning = function(emptyGeneArray_arg) {
 }
 
 
-// This function checks that the user input cohort list is valid:
-checkCohortList = function(cohort_list_arg) {
+// Function to check that the user input cohort list is valid:
+checkCohortList = function(cohortQuery) {
   // List of valid cohorts:
   var validCohortList = ['ACC','BLCA','BRCA','CESC','CHOL','COAD','COADREAD','DLBC','ESCA','FPPP','GBM','GBMLGG','HNSC',
                          'KICH','KIPAN','KIRC','KIRP','LAML','LGG','LIHC','LUAD','LUSC','MESO','OV','PAAD','PCPG','PRAD',
                          'READ','SARC','SKCM','STAD','STES','TGCT','THCA','THYM','UCEC','UCS','UVM'];
 
   // Check the cohort list:
-  numCohorts = cohort_list_arg.length;
+  numCohorts = cohortQuery.length;
   for (var i = 0; i < numCohorts; i++) {
-    var statusTemp = validCohortList.includes(cohort_list_arg[i]);
+    var statusTemp = validCohortList.includes(cohortQuery[i]);
     if (statusTemp == false) {
       return false;
     };
   };
 
   return true;
+};
+
+// Function to count the amount of genes
+amount = function(cohortQuery) {
+  var total = 0;
+  numCohorts = cohortQuery.length;
+  for (var i = 0; i < numCohorts; i++) {
+      total++;
+  };
+  return total;
 };
---
+++ getExpressionDataJSONarray.js
@@ -0,0 +1,31 @@
+// Written by Ethan for VIP WebGen Team
+
+// This JS File contains the function getExpressionDataJSONarray. 
+// To use this function, pass it the arguments: cohortQuery and geneQuery. 
+// The ouput is an array of JSON objects of the form:
+/*
+    {
+      "cohort": string,
+      "expression_log2": float,
+      "gene": string,
+      "geneID": int,
+      "protocol": string,
+      "sample_type": string,
+      "tcga_participant_barcode": string,
+      "z-score": float 
+    }
+*/
+// Going forward, this array of JSONs will likely be the best data structure to use for plotting with d3
+// The array is also sorted by tcga_participant_barcode, which can be thought of as an index for plotting and for 
+// merging this data with other data types (such as mutation data) later on.
+
+// Note: to use getExpressionDataJSONarray, you must set a variable equal to the result returned by the function,
+// then use the .then(function(finalResult)) method to use the finalResult inside the Promise that is returned.
+
+// Function that calls fetchExpressionData and returns the useful format of the data:
+getExpressionDataJSONarray  = async function(cohortQuery, geneQuery) {
+  var dataFetched = await fetchExpressionData(cohortQuery,geneQuery);
+  var results = dataFetched.mRNASeq;
+
+  return await results;
+};
